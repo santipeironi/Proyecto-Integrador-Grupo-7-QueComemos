97734f241e25d824145195a21dfbb555d4ea92c5
SilviaCarinaMonzon <107002180+SilviaCarinaMonzon@users.noreply.github.com>
2022-10-29 15:46:44 -0300

    commit -m "eliminar .rar"

---
+++ modelos/Email.py
@@ -0,0 +1,14 @@
+class Email:
+    Mensaje = ""
+
+    def __init__(self, mensaje):
+        self.Mensaje = mensaje
+
+    def get_Mensaje(self):
+        return self.Mensaje
+
+    def set_Mensaje(self, mensaje):
+        self.Mensaje = mensaje
+    
+    def __str__(self):
+        return f'Mensaje({self.Mensaje})'
